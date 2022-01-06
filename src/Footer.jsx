6119f909ce8644d4fb2a9eaad41c83182ed1efad
Claudia Álvarez <zp2172@zooplus.com>
2022-01-06 18:52:23 +0100

    usecontext in footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { LangContext } from './App';
 import './App.scss';
 
 function Footer() {
+
+  const contextValue = useContext(LangContext);
   return (
     <div className="footer">
       <a className="link" href="https://open.spotify.com/playlist/0UGFY4xQD9tJTAvICvgmxs" target="_blank" rel="noreferrer">{contextValue.dictionary.footer}</a>
